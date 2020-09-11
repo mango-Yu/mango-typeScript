64ce594106cf7cc66c656ae2387fd00110913b88
yuyi.shi <yuyi.shi@scienjoy.com>
2020-09-11 16:54:01 +0800

    init obj

---
+++ dist/demo.js
@@ -0,0 +1,23 @@
+/**
+ * 布尔值
+ * */
+var isDone = false;
+/**
+ * 数字
+  @author syy
+  2020-09-10
+ */
+var decLiteral = 3;
+var hexLiteral = 0xf00d;
+var binaryLiteral = 10;
+var octalLiteral = 484;
+/**
+ * 字符串
+  @author syy
+  2020-09-10
+ */
+var Name = "Gene";
+var age = 37;
+var sentence = "Hello, my name is " + Name + ".\n\nI'll be " + (age + 1) + " years old next month.";
+console.log(sentence);
+//# sourceMappingURL=demo.js.map
---
+++ src/变量/demo.ts
@@ -0,0 +1,32 @@
+/**
+ * 布尔值
+ * */
+let isDone: boolean = false;
+
+
+
+
+
+/**
+ * 数字
+  @author syy
+  2020-09-10
+ */
+let decLiteral: number = 3;
+let hexLiteral: number = 0xf00d;
+let binaryLiteral: number = 0b1010;
+let octalLiteral: number = 0o744;
+
+
+/**
+ * 字符串
+  @author syy
+  2020-09-10
+ */
+
+let Name: string = `Gene`;
+let age: number = 37;
+let sentence: string = `Hello, my name is ${ Name }.
+
+I'll be ${ age + 1 } years old next month.`;
+console.log(sentence)
